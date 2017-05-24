5adca1d68b7e31ff36e690b7861a420fac22b3c4
rencaixia_ss@163.com <sai@saideAir.lan>
2017-05-24 20:37:52 +0800

    1111

--- util.js
+++ util.js
@@ -3,3 +3,7 @@
 export const isJS = (file: string): boolean => /\.js(\?[^.]+)?$/.test(file)
 
 export const isCSS = (file: string): boolean => /\.css(\?[^.]+)?$/.test(file)
+
+array = [1, 2, 3, 4]
+array.sort();
+console.log(array);
